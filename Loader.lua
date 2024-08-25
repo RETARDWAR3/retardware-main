3fec86f180dabeef0ccc519955223a71524091c9
RETARDWAR3 <retardware@gmail.com>
2024-08-25 21:41:27 +0530

    Updated Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -17,7 +17,8 @@ elseif GameId == 13822889 then
 elseif GameId == 8726743209 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RETARDWAR3/retardware-main/main/Refinery-caves.lua"))()
 elseif GameId == 12196278347 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/RETARDWAR3/retardware-main/main/Refinery-Caves-2.lua"))()
+    game.Players.LocalPlayer:Kick("Kicked for safety reasons, script is not 100% safe, if you still want to use it, use the loadstring attached to clipboard seperately, WE ARE NOT RESPONSIBLE FOR A BAN")
+    setclipboard('loadstring(game:HttpGet("https://raw.githubusercontent.com/RETARDWAR3/retardware-main/main/Refinery-Caves-2.lua"))()')
 elseif GameId == 17121106661 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RETARDWAR3/retardware-main/main/Scream-Stream.lua"))()
 elseif GameId == 16426795556 then
@@ -29,7 +30,7 @@ elseif GameId == 606849621 then
 elseif GameId == 2317712696 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RETARDWAR3/retardware-main/main/The-Wild-West.lua"))()
 --// Eclipse/Homohack
-elseif GameID == 113491250 or 115272207 or 1168263273 then
+elseif GameID == 6035872082 or 113491250 or 115272207 then
     setclipboard("https://discord.gg/5BvhKNBUTc")
     OrionLib:MakeNotification({
         Name = "Not our script",
@@ -56,6 +57,16 @@ else
         Name = "We dont know this one",
         Content = "feel free to suggest this game in our discord! (link has been copied to your clipboard)",
         Image = "rbxassetid://4483345998",
-        Time = 8
+        Time = 4
     })
+        OrionLib:MakeNotification({
+        Name = "Eclipse.lol",
+        Content = "Loading universal...",
+        Image = "rbxassetid://4483345998",
+        Time = 4
+    })
+    task.wait(8)
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua"))()
+    Library:Notify("You are not in a homohack supported game, loading universal..")
+    loadstring(Fetch("https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/universal.lua"))()
 end
