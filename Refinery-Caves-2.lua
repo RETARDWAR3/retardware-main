c8a7a1268b45b3ef81aefcfdf6f1b1e38f67a876
RETARDWAR3 <retardware@gmail.com>
2024-09-01 23:14:00 +0530

    aa

--- Refinery-Caves-2.lua
+++ Refinery-Caves-2.lua
@@ -1,1 +1,15 @@
+--IMPORTANT
+local folderName = "RetardwareStorage"
+local workspace = game.Workspace
+-- Check if the folder already exists
+local existingFolder = workspace:FindFirstChild(folderName)
+if not existingFolder then
+    -- Folder does not exist, create it
+    local folder = Instance.new("Folder")
+    folder.Name = folderName
+    folder.Parent = workspace
+end
+
+task.wait(2)
+
 game.Players.LocalPlayer:Kick("You have been kicked because the script is no longer safe. Please use the previous version if you want to continue using it. WE ARE NOT HELD RESPONSIBLE.")
