98322819bd976f059cf0aa7679fe1fe0645539d2
RETARDWAR3 <retardware@gmail.com>
2024-11-20 15:35:37 +0530

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,3 +1,10 @@
+export const manifest = {
+    name: "View Icons",
+    description: "View server or user icons in full resolution.",
+    version: "1.0.0",
+    authors: [{ name: "YourName", id: "YourDiscordID" }],
+};
+
 import { React } from "@vendetta/metro/common";
 import { showToast } from "@vendetta/ui";
 import { openModal } from "@vendetta/ui/components";
@@ -6,61 +13,55 @@ import { getAssetUrl } from "@vendetta/utils";
 
 // Function to fetch and display the icon
 const fetchIcon = (iconUrl) => {
-  if (!iconUrl) {
-    showToast("No icon available!", "error");
-    return;
-  }
+    if (!iconUrl) {
+        showToast("No icon available!", "error");
+        return;
+    }
 
-  openModal(() => (
-    <ImageModal imageUrl={iconUrl} />
-  ));
+    openModal(() => (
+        <ImageModal imageUrl={iconUrl} />
+    ));
 };
 
 // Image modal component
 const ImageModal = ({ imageUrl }) => {
-  const styles = {
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "rgba(0, 0, 0, 0.8)",
-    },
-    image: {
-      width: "80%",
-      height: "80%",
-      resizeMode: "contain",
-    },
-  };
+    const styles = {
+        container: {
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(0, 0, 0, 0.8)",
+        },
+        image: {
+            width: "80%",
+            height: "80%",
+            resizeMode: "contain",
+        },
+    };
 
-  return (
-    <div style={styles.container}>
-      <img src={imageUrl} alt="Icon" style={styles.image} />
-    </div>
-  );
+    return (
+        <div style={styles.container}>
+            <img src={imageUrl} alt="Icon" style={styles.image} />
+        </div>
+    );
 };
 
 export default {
-  name: "View Icons",
-  description: "View server or user icons in full resolution.",
-  version: "1.0.0",
-  author: "YourName",
+    onLoad: () => {
+        const { UserProfile, GuildProfile } = findByProps("UserProfile", "GuildProfile");
 
-  onLoad: () => {
-    const { UserProfile, GuildProfile } = findByProps("UserProfile", "GuildProfile");
+        UserProfile.addContextMenuHandler((user) => {
+            fetchIcon(getAssetUrl(user.avatarURL));
+        });
 
-    UserProfile.addContextMenuHandler((user) => {
-      fetchIcon(getAssetUrl(user.avatarURL));
-    });
+        GuildProfile.addContextMenuHandler((guild) => {
+            fetchIcon(getAssetUrl(guild.iconURL));
+        });
+    },
 
-    GuildProfile.addContextMenuHandler((guild) => {
-      fetchIcon(getAssetUrl(guild.iconURL));
-    });
-  },
-
-  onUnload: () => {
-    // Cleanup any attached event handlers
-    const { UserProfile, GuildProfile } = findByProps("UserProfile", "GuildProfile");
-    UserProfile.removeContextMenuHandler();
-    GuildProfile.removeContextMenuHandler();
-  },
+    onUnload: () => {
+        const { UserProfile, GuildProfile } = findByProps("UserProfile", "GuildProfile");
+        UserProfile.removeContextMenuHandler();
+        GuildProfile.removeContextMenuHandler();
+    },
 };
